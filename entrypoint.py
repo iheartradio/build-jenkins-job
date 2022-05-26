b0a3ae6ef5dfb64dd7b79af7d59bd969ef78c09c
Taylor Perkins <taylorperkins@iheartmedia.com>
2022-05-26 17:13:00 -0500

    Initial commit

--- entrypoint.py
+++ entrypoint.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python3
+import base64
+import json
+import sys
+import time
 
 # use of https://python-jenkins.readthedocs.io/en/latest/index.html
-
-import sys
-import requests
 import jenkins
-import time
-import json
 
 
 def mandatory_arg(argv):
@@ -23,47 +22,54 @@ JOB_PATH = mandatory_arg(sys.argv[4])
 
 # not mandatory
 JOB_PARAMS = sys.argv[5] or '{}'
-
-# create/connect jenkins server
-server = jenkins.Jenkins(f"http://{JENKINS_URL}", username=JENKINS_USER, password=JENKINS_TOKEN)
-user = server.get_whoami()
-version = server.get_version()
-print(f"Hello {user['fullName']} from Jenkins {version}")
-
-# build job
-split = JOB_PATH.split("job/")
-job_name = "".join(split)
-server.build_job(job_name, parameters=json.loads(JOB_PARAMS), token=JENKINS_TOKEN)
-queue_info = server.get_queue_info()
-queue_id = queue_info[0].get('id')
-
-# define url to request build_number
-url = f"http://{JENKINS_USER}:{JENKINS_TOKEN}@{JENKINS_URL}/queue/item/{queue_id}/api/json?pretty=true"
+INCLUDE_LOGS = sys.argv[6] or False
 
 
-def get_trigger_info(url: str):
-    trigger_info = requests.get(url).json()
-    return trigger_info
+class JenkinsProxy:
+
+    def __init__(self):
+        self.server = jenkins.Jenkins(f"http://{JENKINS_URL}", username=JENKINS_USER, password=JENKINS_TOKEN)
+
+    @property
+    def job_name(self) -> str:
+        return "".join(JOB_PATH.split("job/"))
+
+    def build_job(self):
+        return self.server.build_job(self.job_name, parameters=json.loads(JOB_PARAMS), token=JENKINS_TOKEN)
+
+    def get_queue_item(self, item_id):
+        while True:
+            item = self.server.get_queue_item(item_id)
+            if "executable" in item:
+                return item
+
+            time.sleep(3)
+
+    def poll_build(self, build_number: int):
+        while True:
+            build_info = self.server.get_build_info(name=self.job_name, number=build_number)
+            if not build_info["result"]:
+                time.sleep(1)
+                continue
+            return build_info
 
 
-while "executable" not in (info := get_trigger_info(url)):
-    time.sleep(3)
+jp = JenkinsProxy()
+queue_item_id = jp.build_job()
+queue_item = jp.get_queue_item(queue_item_id)
 
-build_number = info["executable"]["number"]
-print(f"BUILD NUMBER: {build_number}")
+info = jp.poll_build(build_number=queue_item["executable"]["number"])
 
+if INCLUDE_LOGS:
+    log_info = jp.server.get_build_console_output(jp.job_name, info['number'])
+    log_info = base64.b64encode(log_info.encode('utf-8'))
+    print(f"::set-output name=job_log_info::{log_info}")
+else:
+    print(f"::set-output name=job_log_info::")
 
-def get_status(name: str, number: int) -> str:
-    build_info = server.get_build_info(name=name, number=number)
-    job_status = build_info["result"]
-    return job_status
+print(f"::set-output name=job_status::{info['result']}")
+print(f"::set-output name=job_number::{info['number']}")
+print(f"::set-output name=job_url::{info['url']}")
 
-
-while not (status := get_status(job_name, build_number)):
-    time.sleep(1)
-
-print(f"Job status is : {status}")
-print(f"::set-output name=job_status::{status}")
-
-if status != 'SUCCESS':
-    exit(1)
+if info['result'] != "SUCCESS":
+    sys.exit(1)
